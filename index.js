820ca1cb33f87a8373361729bfe55bc867e4b705
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-10 17:55:20 -0600

    error connecting to database

--- index.js
+++ index.js
@@ -11,12 +11,9 @@ Promise.resolve()
   .then(() => app.listen(PORT, () => console.log(`App listening on port ${PORT}`)))
   .catch((err) => { if (NODE_ENV === 'development') console.error(err.stack); });
 
-// ROUTES
-app.get('/films/:id/recommendations', getFilmRecommendations);
-
-//connect to db
+// CONNECT TO DB
 const DB = new Sequelize(`sqlite:${process.env.DB_PATH}`);
-//test connection (http://docs.sequelizejs.com/manual/installation/getting-started.html)
+// TEST CONNECTION (http://docs.sequelizejs.com/manual/installation/getting-started.html)
 DB
   .authenticate()
   .then(() => {
@@ -25,34 +22,55 @@ DB
   .catch(err => {
     console.error('Unable to connect to the database:', err);
   });
+// DB models
+// const films = DB.define('films');
+// films.sync().then(() => {
+//   films.all().then(films => {
+//     console.log(films);
+//   })
+// }).catch(err => {
+//   console.log(err);
+//   console.log('db doesnot sync');
+// })
+DB.sync();
+
+// films.all().then(films=> {
+//   console.log(films);
+// })
+
+
+
+// ROUTES
+app.get('/films/:id/recommendations', getFilmRecommendations);
+
+
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
-  
   // get films from database with same genre, released within 15 years sorted by film id
-  /* SQLite query within DB
-  
-  DB.query()
-  SELECT 
-    films.id AS id,
-    title,
-    release_date AS releaseDate,
-    genres.name AS genre
-  FROM films
-  JOIN genres ON genres.id = films.genre_id
-  WHERE 
-    genres.id = (SELECT genre_id FROM films WHERE id = $1)
-    AND cast(release_date AS string) > cast((SELECT release_date FROM films WHERE id = $1) - '15' AS string)
-    AND cast(release_date AS string) < cast((SELECT release_date FROM films WHERE id = $1) + '15' AS string)
-  ORDER BY films.id ASC;
+  DB.query(`
+    SELECT 
+      films.id AS id,
+      title,
+      release_date AS releaseDate,
+      genres.name AS genre
+    FROM 'films'
+    JOIN genres ON genres.id = films.genre_id
+    WHERE 
+      genres.id = (SELECT genre_id FROM 'films' WHERE id = $1)
+      AND cast(release_date AS string) > cast((SELECT release_date FROM 'films' WHERE id = $1) - '15' AS string)
+      AND cast(release_date AS string) < cast((SELECT release_date FROM 'films' WHERE id = $1) + '15' AS string)
+    ORDER BY films.id ASC;`,
+    { bind: [req.params.id], type: DB.QueryTypes.SELECT}
+  ).then(films => {
+    console.log(films); // check films accurately retrieved
+    // check reviews from each film and pop from array if < 5 reviews and average rating <= 4.0
+    // by using outside api; save averageRating on object and reviews (num reviews) if good
+  }).catch(err => {
+    console.log(err);
+    res.status(500).send('Database error');
+  })
 
-  */
-
-  // check reviews from each film and pop from array if < 5 reviews and average rating <= 4.0
-  // by using outside api; save averageRating on object and reviews (num reviews) if good
-
-
-  res.status(500).send('Not Implemented');
 }
 
 module.exports = app;
