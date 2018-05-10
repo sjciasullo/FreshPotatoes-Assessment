abeaa33e2a8164c737ecb3143ba659f50499bcaf
Steven Ciasullo <sjciasullo@gmail.com>
2018-05-10 14:54:06 -0600

    define strategy within getFilms

--- index.js
+++ index.js
@@ -16,6 +16,33 @@ app.get('/films/:id/recommendations', getFilmRecommendations);
 
 // ROUTE HANDLER
 function getFilmRecommendations(req, res) {
+  // connect to database using DB_PATH and Sequelize
+  const DB = new Sequelize('sqlite:./db/database.db');
+
+
+  // get films from database with same genre, released within 15 years sorted by film id
+  /* SQLite query within DB
+  
+  DB.query()
+  SELECT 
+    films.id AS id,
+    title,
+    release_date AS releaseDate,
+    genres.name AS genre
+  FROM films
+  JOIN genres ON genres.id = films.genre_id
+  WHERE 
+    genres.id = (SELECT genre_id FROM films WHERE id = $1)
+    AND cast(release_date AS string) > cast((SELECT release_date FROM films WHERE id = $1) - '15' AS string)
+    AND cast(release_date AS string) < cast((SELECT release_date FROM films WHERE id = $1) + '15' AS string)
+  ORDER BY films.id ASC;
+
+  */
+
+  // check reviews from each film and pop from array if < 5 reviews and average rating <= 4.0
+  // by using outside api; save averageRating on object and reviews (num reviews) if good
+
+
   res.status(500).send('Not Implemented');
 }
 
